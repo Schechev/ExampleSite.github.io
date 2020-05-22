6bf12b12a6b39b26d4e9dd587e6911d711fa9737
Schechev <60988463+Schechev@users.noreply.github.com>
2020-05-22 09:44:52 +0300

    version1.1

--- app.js
+++ app.js
@@ -1,52 +1,48 @@
 
 /*SLIDER=====================*/
 
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
-
 
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace("active", "");
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
 
 /*VIDEO*/
-var vid = document.getElementById("video"); 
-var button = document.getElementById("button");
+let vid = document.getElementById("video"); 
+let button = document.getElementById("button");
 
 button.addEventListener("click", function(){
   if(vid.paused){
     vid.play();
     button.innerHTML = "||";
+    setTimeout(function(){document.getElementById("button").style.opacity="0";}, 1000);
   } else {
     vid.pause();
     button.innerHTML = ">";
   }
 });
-function myMoveFunction() {
-  
-  document.getElementById("button").style.display="block";
-  setTimeout(function(){ document.getElementById("button").style.display="none"}, 2500);
-  
-}
-
-
 
 function mouseOut() {
-  document.getElementById("button").style.display="none";
-}
+  document.getElementById("button").style.opacity="0";
+};
+
+function mouseIn() {
+document.getElementById("button").style.opacity="1";
+ };
